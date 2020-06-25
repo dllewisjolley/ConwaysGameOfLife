664a1d2acc26b1ef5dedc3cb2704ec2c831e8b70
dlewisjolley <dlewisjolley@gmail.com>
2020-06-25 15:27:56 -0400

    initial commit

---
+++ GameOfLife/GameOfLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Lambda_School_Loaner_148 on 6/25/20.
+//  Copyright © 2020 Diante Lewis-Jolley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
